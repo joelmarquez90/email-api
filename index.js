9b94540326b039f3f168489eefe5c26926ec4281
Joel Marquez <90joelmarquez@gmail.com>
2017-02-04 13:53:28 -0300

    Initial comit

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var SparkPost = require('sparkpost');
+var pjson = require('./package.json');
+
+var client = new SparkPost(process.ENV.SPARKPOST_API_KEY);
+
+var allowCrossDomainConfig = function(req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+  if ('OPTIONS' === req.method) {
+    return res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomainConfig);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*
+  Returns the version of this API
+*/
+app.get('/', function (req, res) {
+  return res.send({ version: pjson.version });
+});
+
+/* You should send this parameters in the body:
+  {
+    "name": "My Name",
+    "email": "myemail@gmail.com",
+    "phone": "1234567890",
+    "message": "My Message"
+  }
+*/
+app.post('/email', function (req, res) {
+
+  var name = req.body.name || '';
+  var email = req.body.email || '';
+  var phone = req.body.phone || '';
+  var message = req.body.message || '';
+
+  var from = process.ENV.EMAIL_FROM || '';
+  var to = process.ENV.EMAIL_TO || '';
+
+  client.transmissions.send({
+    content: {
+      from: from,
+      subject: 'Contact: ' + name,
+      text:'Name: ' + name + '\nEmail: ' + email + '\nPhone number: ' + phone + '\n\n' + message
+    },
+    recipients: [
+      {address: to}
+    ]
+  })
+  .then(data => {
+    res.sendStatus(200);
+  })
+  .catch(err => {
+    res.sendStatus(400);
+  });
+});
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Email API running!');
+});
